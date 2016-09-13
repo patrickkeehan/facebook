5b0173a23025be0ed855aff4cda5d4dc639a7a29
Patrick Keehan <patrick.keehan@gmail.com>
2016-09-13 16:07:11 +0100

    Added to the fields we're requesting

--- FacebookStrategy.php
+++ FacebookStrategy.php
@@ -129,7 +129,7 @@ class FacebookStrategy extends OpauthStrategy{
 	 * @return array Parsed JSON results
 	 */
 	private function me($access_token){
-		$me = $this->serverGet('https://graph.facebook.com/me', array('access_token' => $access_token,'fields' => 'email'), null, $headers);
+		$me = $this->serverGet('https://graph.facebook.com/me', array('access_token' => $access_token,'fields' => 'first_name,last_name,email'), null, $headers);
 		if (!empty($me)){
 			return json_decode($me);
 		}
